ab5a053f1523489b1803bbd93ba5cb35e32b547c
Irakli Gozalishvili <contact@gozala.io>
2020-09-04 11:22:09 -0700

    chore: change up exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { CID } from './cid.js'
 import varint from './varint.js'
 import * as bytes from './bytes.js'
-import { ImplicitSha256Digest, Digest, Hasher } from './hashes/hasher.js'
+import * as codec from './codecs/codec.js'
+import * as base from './bases/base.js'
+import * as hash from './hashes/hasher.js'
 
-export { ImplicitSha256Digest, Digest, Hasher, CID, varint, bytes }
+export { CID, varint, bytes, codec, base, hash }
